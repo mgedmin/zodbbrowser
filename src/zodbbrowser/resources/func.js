2f4c554c6cd3b3e94cea5d70b5db2051a90f7af1
Marius Gedminas <marius@gedmin.as>
2009-07-08 16:07:56 +0300

    Fiddle with the look and feel of expanders.

--- src/zodbbrowser/resources/func.js
+++ src/zodbbrowser/resources/func.js
@@ -1,14 +1,11 @@
-$(document).ready(
-    function() {
-        expand = function (caller) {
-            content = $(caller).parent().next()
-            if (content.is(':hidden')) {
-                content.show()
-                $(caller).attr('src', '@@/collapse.png')
-            } else {
-                content.hide()
-                $(caller).attr('src', '@@/expand.png')
-            }
-        }
+function expand(caller) {
+    var content = $(caller).next();
+    var icon = $(caller).children('img');
+    if (content.is(':hidden')) {
+        content.show();
+        $(icon).attr('src', '@@/collapse.png');
+    } else {
+        content.hide();
+        $(icon).attr('src', '@@/expand.png');
     }
-);
+}
