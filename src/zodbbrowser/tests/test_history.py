6fb4ef4e79ea20602e4bf47349c029bf0a1715d2
Marius Gedminas <marius@gedmin.as>
2009-07-16 22:39:23 +0300

    Unit tests for loadState.

--- src/zodbbrowser/tests/test_history.py
+++ src/zodbbrowser/tests/test_history.py
@@ -2,8 +2,13 @@ import unittest
 import sys
 
 import transaction
+from persistent import Persistent
 from zodbbrowser.tests.realdb import RealDatabaseTest
-from zodbbrowser.history import getHistory
+from zodbbrowser.history import getHistory, loadState
+
+
+class PersistentObject(Persistent):
+    pass # we need a subclass so we get a __dict__
 
 
 class TestFileStorage(RealDatabaseTest):
@@ -24,6 +29,48 @@ class TestFileStorage(RealDatabaseTest):
             transaction.commit()
         history = getHistory(obj)
         self.assertEquals(len(history), 11)
+
+
+class TestLoadState(RealDatabaseTest):
+
+    def setUp(self):
+        RealDatabaseTest.setUp(self)
+        root = self.conn.root()
+        self.adam = root['adam'] = PersistentObject()
+        transaction.commit()
+        self.eve = root['eve'] = PersistentObject()
+        transaction.commit()
+        self.adam.laptop = 'ThinkPad T23'
+        transaction.commit()
+        self.eve.laptop = 'MacBook'
+        transaction.commit()
+        self.adam.laptop = 'ThinkPad T42'
+        transaction.commit()
+        self.adam.laptop = 'ThinkPad T61'
+        transaction.commit()
+
+    def test_latest_state(self):
+        state = loadState(self.adam)
+        self.assertEquals(state, dict(laptop='ThinkPad T61'))
+
+    def test_exact_state(self):
+        tid = getHistory(self.adam)[1]['tid']
+        state = loadState(self.adam, tid)
+        self.assertEquals(state, dict(laptop='ThinkPad T42'))
+
+    def test_earlier_state(self):
+        tid = getHistory(self.eve)[0]['tid']
+        state = loadState(self.adam, tid)
+        self.assertEquals(state, dict(laptop='ThinkPad T23'))
+
+    def test_error_handling(self):
+        tid = getHistory(self.adam)[-1]['tid']
+        try:
+            loadState(self.eve, tid)
+        except Exception, e:
+            self.assertTrue("did not exist in or before" in str(e))
+        else:
+            self.fail("did not raise")
 
 
 def test_suite():
