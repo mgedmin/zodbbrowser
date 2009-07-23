730d5e3cb46c1a0e5783efee31fa7de50472a7a0
Marius Gedminas <marius@gedmin.as>
2009-07-23 18:43:11 +0300

    Add a second functional test.

--- src/zodbbrowser/ftests/test_standalone.py
+++ src/zodbbrowser/ftests/test_standalone.py
@@ -6,7 +6,10 @@ import unittest
 import threading
 import asyncore
 
+from ZODB.POSException import ReadOnlyError
 from zope.testbrowser.browser import Browser
+from zope.app.testing import setup
+
 from zodbbrowser.standalone import main, serve_forever, stop_serving
 
 
@@ -19,6 +22,7 @@ class StandaloneZodbBrowserTestCase(unittest.TestCase):
     def setUp(self):
         self.port_number = self.findUnusedPort()
         self.url = 'http://localhost:%d' % self.port_number
+        self.serverThread = None
 
     def findUnusedPort(self):
         return 1234 # TODO: be smarter
@@ -38,8 +42,10 @@ class StandaloneZodbBrowserTestCase(unittest.TestCase):
 
     def tearDown(self):
         # signal the server thread to stop serving
-        stop_serving()
-        self.serverThread.join(1.0)
+        if self.serverThread is not None:
+            stop_serving()
+            self.serverThread.join(1.0)
+        setup.placelessTearDown()
 
 
 class TestCanCreateEmptyDataFs(StandaloneZodbBrowserTestCase):
@@ -49,16 +55,23 @@ class TestCanCreateEmptyDataFs(StandaloneZodbBrowserTestCase):
     def setUp(self):
         StandaloneZodbBrowserTestCase.setUp(self)
         self.tempdir = tempfile.mkdtemp('zodbbrowser')
+        self.empty_fs = os.path.join(self.tempdir, 'empty.fs')
 
     def tearDown(self):
         StandaloneZodbBrowserTestCase.tearDown(self)
         shutil.rmtree(self.tempdir)
 
-    def test(self):
-        self.runServer(os.path.join(self.tempdir, 'empty.fs'), '--rw')
+    def test_can_create_empty_data_fs(self):
+        self.runServer(self.empty_fs, '--rw')
         browser = Browser(self.url)
         self.assertTrue('zodbbrowser' in browser.contents)
         self.assertTrue('zope.app.folder.folder.Folder' in browser.contents)
+
+    def test_cannot_start_in_read_only_mode(self):
+        self.assertRaises(ReadOnlyError, self.runServer, self.empty_fs)
+        # Due to a bug in ZODB, the new database *is* created, it just
+        # has no object in it
+        # self.assertTrue(not os.path.exists(self.empty_fs))
 
 
 def test_suite():
