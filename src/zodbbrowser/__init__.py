af7d691155789a23dc628f4fa9392ec610e16033
Marius Gedminas <marius@gedmin.as>
2009-11-23 20:52:57 +0200

    Preparing to release 0.5.

--- src/zodbbrowser/__init__.py
+++ src/zodbbrowser/__init__.py
@@ -1,20 +1,22 @@
 """
 ZODB Browser has the following submodules:
 
-  diff        -- compute differences between two dictionaries
-  testing     -- doodads to make writing tests easier
+  diff         -- compute differences between two dictionaries
+  testing      -- doodads to make writing tests easier
 
-  history     -- extracts historical state information from the ZODB
-  state       -- IStateInterpreter adapters for making sense of unpickled data
-  value       -- IValueRenderer adapters for pretty-printing objects to HTML
+  history      -- extracts historical state information from the ZODB
+  state        -- IStateInterpreter adapters for making sense of unpickled data
+  value        -- IValueRenderer adapters for pretty-printing objects to HTML
 
-  interfaces  -- interface definitions
-  browser     -- browser views
+  btreesupport -- special handling of OOBTree objects
 
-  standalone  -- standalone application that starts a web server
+  interfaces   -- interface definitions
+  browser      -- browser views
+
+  standalone   -- standalone application that starts a web server
 
 """
 
 
-__version__ = '0.5dev'
+__version__ = '0.5'
 __homepage__ = 'http://launchpad.net/zodbbrowser'
