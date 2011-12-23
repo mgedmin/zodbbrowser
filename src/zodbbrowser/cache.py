5e35a63e404ed4284310b1c51fa995b899e1b742
Marius Gedminas <marius@gedmin.as>
2011-12-23 02:05:18 +0200

    Add some caching.
    It doesn't help much when the pain is in the initial load and your database
    has hundreds of thousands of transactions.  (~80 seconds for FileStorage,
    my patience timed out with ZEO).

---
+++ src/zodbbrowser/cache.py
@@ -0,0 +1,40 @@
+"""
+Ad-hoc caching, because uncached zodbbrowser is slow and sad.
+"""
+
+import time
+import weakref
+
+MINUTES = 60
+HOURS = 60 * MINUTES
+
+STORAGE_TIDS = weakref.WeakKeyDictionary()
+
+
+def expired(cache_dict, cache_for):
+    if 'last_update' not in cache_dict:
+        return True
+    return cache_dict['last_update'] > time.time() - cache_for
+
+
+def getStorageTids(storage, cache_for=5*MINUTES):
+    cache_dict = STORAGE_TIDS.setdefault(storage, {})
+    if expired(cache_dict, cache_for):
+        if cache_dict.get('tids'):
+            first = cache_dict['tids'][-1]
+            last = cache_dict['tids'][-1]
+            first_record = next(storage.iterator(), None)
+            if first_record and first_record.tid == first:
+                # okay, look for new transactions appended at the end
+                new = [t.tid for t in storage.iterator(start=last)]
+                if new and new[0] == last:
+                    del new[0]
+                cache_dict['tids'].extend(new)
+            else:
+                # first record changed, we must've packed the DB
+                cache_dict['tids'] = [t.tid for t in storage.iterator()]
+        else:
+            cache_dict['tids'] = [t.tid for t in storage.iterator()]
+        cache_dict['last_update'] = time.time()
+    return cache_dict['tids']
+
