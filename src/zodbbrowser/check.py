e045fa78f00df207d4af3d8fc1d8b439c6394c06
Sylvain Viollon <thefunny@gmail.com>
2014-02-06 09:50:14 +0100

    Refactor code and add docstring. Add a feature to save the computed information into an SQLite database.

--- src/zodbbrowser/check.py
+++ src/zodbbrowser/check.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 import cPickle
 import io
-import itertools
-import gc
 import logging
 import optparse
+import sqlite3
 import sys
-import ZODB.broken
 
-from ZODB.utils import p64, u64
+from ZODB.utils import u64
 from zodbbrowser.standalone import open_database
 
 def referredoids(data):
+    """Analyze a record data an return a set of unique OID referred inside
+    this record.
+    """
     oids = set()
     refs = []
     unpickler = cPickle.Unpickler(io.BytesIO(data))
@@ -28,26 +29,53 @@ def referredoids(data):
             oids.add(ref[1][1])
     return oids
 
-def check(db):
+def analyze_database(db):
+    """Analyze a Zope database and return two dictionnary: one called
+    forward_references containing which OID refers which other and a
+    second one called backward_references containing a list of which
+    OID are referred by which other.
+
+    """
     forward_references = {}
-    back_references = {}
+    backward_references = {}
 
     for transaction in db._storage.iterator():
         for record in transaction:
-            refs = map(u64, referredoids(record.data))
-            forward_references[u64(record.oid)] = refs
-            for oid in refs:
-                back_references[oid] = u64(record.oid)
+            current_oid = u64(record.oid)
+            referred_oids = map(u64, referredoids(record.data))
+            forward_references[current_oid] = referred_oids
+            for oid in referred_oids:
+                backward_references.setdefault(oid, set([])).add(current_oid)
 
-    forward = set(forward_references.iterkeys())
-    back = set(back_references.iterkeys())
-    broken = back - forward
-    if broken:
-        # We have broken objects
-        print broken
-        sys.exit(1)
-    print 'no broken objects'
-    sys.exit(0)
+    return forward_references, backward_references
+
+def save_references(db_name, forward_references, backward_references):
+    """Save forward and backward references into an SQLite database.
+    """
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    cursor.execute("""
+CREATE TABLE IF NOT EXISTS forward_references
+(source_oid BIGINT, target_oid BIGINT)
+    """)
+    cursor.execute("""
+CREATE TABLE IF NOT EXISTS backward_references
+(source_oid BIGINT, target_oid BIGINT)
+    """)
+    for source_oid, target_oids in forward_references.iteritems():
+        for target_oid in target_oids:
+            cursor.execute("""
+INSERT INTO forward_references (source_oid, target_oid) VALUES
+({0}, {1})
+            """.format(source_oid, target_oid))
+    for source_oid, target_oids in backward_references.iteritems():
+        for target_oid in target_oids:
+            cursor.execute("""
+INSERT INTO backward_references (source_oid, target_oid) VALUES
+({0}, {1})
+            """.format(source_oid, target_oid))
+    connection.commit()
+    connection.close()
 
 def main(args=None):
     logging.basicConfig(format="%(message)s")
@@ -68,12 +96,32 @@ def main(args=None):
                       help='connect to given ZEO storage')
     parser.add_option('--db', metavar='DATA.FS',
                       help='use given Data.fs file')
+    parser.add_option('--save', metavar='FILE.DB',
+                      help='save computed information for reuse')
     parser.add_option('--rw', action='store_false', dest='readonly',
                       default=True,
                       help='open the database read-write (default: read-only)')
     opts, args = parser.parse_args(args)
-    db = open_database(opts)
-    check(db)
+    try:
+        db = open_database(opts)
+    except ValueError as e:
+        parser.error(e.msg)
+
+    forward_references, backward_references = analyze_database(db)
+
+    if opts.save:
+        save_references(opts.save, forward_references, backward_references)
+
+    found_oids = set(forward_references.iterkeys())
+    referred_oids = set(backward_references.iterkeys())
+    broken_oids = referred_oids - found_oids
+    if broken_oids:
+        # We have broken objects
+        print '{0} broken objects'.format(len(broken_oids))
+        sys.exit(1)
+    print 'no broken objects'
+    sys.exit(0)
+
 
 if __name__ == '__main__':
     main()
