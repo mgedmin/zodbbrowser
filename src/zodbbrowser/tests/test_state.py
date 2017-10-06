aa05ddc39643cffa38dd755a35544270d905c0d0
Marius Gedminas <marius@gedmin.as>
2017-10-06 11:54:18 +0300

    Python 3: no dict.items()[0]

--- src/zodbbrowser/tests/test_state.py
+++ src/zodbbrowser/tests/test_state.py
@@ -64,7 +64,8 @@ class TestZodbObjectState(RealDatabaseTest):
     def testZodbObjectState(self):
         state = ZodbObjectState(self.obj)
         self.assertEquals(state.listItems(), None)
-        self.assertTrue(state.listAttributes()[0][0], '_SampleContainer__data')
+        self.assertEqual(list(state.listAttributes())[0][0],
+                         '_SampleContainer__data')
         self.assertEquals(state.getParent(), None)
         self.assertEquals(state.getName(), None)
         self.assertTrue('_SampleContainer__data' in state.asDict().keys())
